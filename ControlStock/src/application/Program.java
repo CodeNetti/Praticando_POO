372bf01dd2664b7660b0dd7e0aae1bf9052b14ee
Raphael Simonetti <raphasimonettti@gmail.com>
2023-09-07 05:03:00 -0300

    Utilizando Construtores

--- ControlStock/src/application/Program.java
+++ ControlStock/src/application/Program.java
@@ -7,40 +7,41 @@ import myShop.Product;
 
 public class Program {
 
-	
 	public static void main(String[] args) {
-		Product prod = new Product();
+		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Por favor coloque os dados do produto");
 		System.out.println("Qual é o nome do produto?");
-		prod.name = sc.nextLine();
+		 String name = sc.nextLine();
 		System.out.println("Qual é o preço do produto?");
-		prod.price = sc.nextDouble();
+		double price = sc.nextDouble();
 		System.out.println("Qual é a quantidade em estoque?");
-		prod.quantity = sc.nextInt();
+		int quantity = sc.nextInt();
 		
-		
-		
-		System.out.println("Dados do Produto, " + prod.name + ", $" + prod.price + " , " + prod.quantity + " Unidades, Total: $ " + (prod.quantity * prod.price));
-	
+		Product prod = new Product(name, price ,quantity);
+
+		System.out.println("Dados do Produto, " + prod.name + ", $" + prod.price + " , " + prod.quantity
+				+ " Unidades, Total: $ " + (prod.quantity * prod.price));
+
 		System.out.println("Digite a quantidade de produtos que deseja inserir no estoque");
-		
+
 		int insertProd = sc.nextInt();
-		
+
 		prod.AddProducts(insertProd);
-		
-		System.out.println("Estoque de " + prod.name + ", Unidades: " + prod.quantity + " Total: $" + (prod.quantity * prod.price));
-		
+
+		System.out.println("Estoque de " + prod.name + ", Unidades: " + prod.quantity + " Total: $"
+				+ (prod.quantity * prod.price));
+
 		System.out.println("Digite a quantidade de produtos que deseja retirar do estoque");
-		
+
 		int removeProd = sc.nextInt();
-		
+
 		prod.RemoveProducts(removeProd);
-		
-		System.out.println("Estoque de " + prod.name + ", Unidades: " + prod.quantity + " Total: $" + (prod.quantity * prod.price));
-		
-		
+
+		System.out.println("Estoque de " + prod.name + ", Unidades: " + prod.quantity + " Total: $"
+				+ (prod.quantity * prod.price));
+
 		sc.close();
 
 	}
--- ControlStock/src/myShop/Product.java
+++ ControlStock/src/myShop/Product.java
@@ -2,9 +2,16 @@ package myShop;
 
 public class Product {
 	 public String name;
-	 public double price;
+	public double price;
 	 public int quantity;
 	 public double total;
+	 
+	 public Product(String name, double price, int quantity) {
+			super();
+			this.name = name;
+			this.price = price;
+			this.quantity = quantity;
+		}
 	
 	
 	public double TotalValueinStock() 
